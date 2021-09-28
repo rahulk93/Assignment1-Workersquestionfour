9fbb4bfc2308ecf38105778664380d9eb4ef6a9e
Rahul Khatri <forever_rahul16@hotmail.com>
2021-09-28 14:08:24 +1300

    Add project files.

---
+++ Workersquestionfour/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workersquestionfour
+{
+    class Employee
+    {
+        // The four things below are auto-implemented properties. 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int YearStarted { get; set; }
+        public int YearsWorked { get; set; }//This auto-implemented property is important because it assists the system how many years a employee worked in a company. 
+        
+        //This below is parameteried constructor with string firstName and string lastName. Furthermore display message "Creating new Employee record...".
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Console.WriteLine("Creating new Employee record...");
+        }
+
+        public string GetFullName()
+        {
+            return FirstName + LastName;
+        }
+        // The 'Date.Time is imporant is imporant because it helps to calculate number of years that a employee worked.
+        public int GetYearsWorked()
+        {
+            YearsWorked = DateTime.Now.Year - YearStarted;
+            return YearsWorked;
+            
+        }
+
+        
+
+        
+
+      
+    }
+}
---
+++ Workersquestionfour/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workersquestionfour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee("firstName","lastName");//New object created with two strings firstName and lastName variables in it. This is because I have created paratermised Employee constructor at Employee class.
+
+            char choice;// The "char choice" is an important variable since it assists to genrate while loop.
+
+            Console.WriteLine("Enter details of new employee below:");
+            Console.Write("First name: ");
+            e1.FirstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            e1.LastName = Console.ReadLine();
+            
+            Console.Write("\nYear employee started working for the company?: ");
+            e1.YearStarted = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("\n\n**********************************************************");
+            Console.WriteLine("Employee {0} {1}, has worked for the company for {2} years",e1.FirstName,e1.LastName, e1.GetYearsWorked());
+
+            Console.WriteLine("\nDo you want to enter another employee? (y/n) ");
+            choice = Convert.ToChar(Console.ReadLine());
+
+            while (choice == 'y' || choice == 'Y')// This line of code on the left is from https://www.youtube.com/watch?v=JTCDnty-RVQ website.
+            {
+                Console.WriteLine("Creating new Employee record...");
+                Console.WriteLine("Enter details of new employee below:");
+                Console.Write("First name: ");
+                e1.FirstName = Console.ReadLine();
+                Console.Write("Last Name: ");
+                e1.LastName = Console.ReadLine();
+                
+
+                Console.Write("\nYear employee started working for the company?: ");
+                e1.YearStarted = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("\n\n**********************************************************");
+                Console.WriteLine("Employee {0} {1}, has worked for the company for {2} years", e1.FirstName, e1.LastName, e1.GetYearsWorked());
+
+                Console.WriteLine("\nDo you want to enter another employee? (y/n) ");
+                choice = Convert.ToChar(Console.ReadLine());
+
+            }
+
+            if (choice == 'n' || choice == 'N')// This line of code on the left  is from https://www.youtube.com/watch?v=JTCDnty-RVQ website.
+            {
+                Console.WriteLine("\n\npress any key to exit the program");
+            }
+
+            Console.ReadKey();
+
+            //This program is useful for the companies since it staff members to calculate of how many years they have been working there.- Rahul Khatri
+
+
+
+
+        }
+    }
+}
